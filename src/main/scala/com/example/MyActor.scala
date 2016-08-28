a0706e924413ef1cd0e441a0e0ff3d0189f00ffe
shigemk2 <i.am.shige@gmail.com>
2016-08-29 00:58:26 +0900

    Add MyActor

--- src/main/scala/com/example/MyActor.scala
+++ src/main/scala/com/example/MyActor.scala
@@ -1,5 +1,40 @@
 package com.example
 
-class MyActor {
+import akka.actor.{Actor, Props}
+import akka.event.Logging
 
+class MyActor extends Actor {
+  val log = Logging(context.system, this)
+  val child = context.actorOf(Props[MyActor2], name = "myChild")
+
+  override def preStart(): Unit = {
+    log.info("preStart")
+  }
+
+  override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+    log.info("preRestart")
+    if (message.nonEmpty)
+      log.info("last message was: %s" format message)
+    context.stop(child)
+  }
+
+  override def postRestart(reason: Throwable): Unit = {
+    log.info("postRestart")
+  }
+
+  override def postStop(): Unit = {
+    log.info("postStop")
+  }
+
+  def receive = {
+    case "crash" => {
+      throw new Exception
+    }
+    case s: String => {
+      log.info(s)
+      child ! s
+    }
+    case _ => {
+    }
+  }
 }
