ad158e4d62d32c625345d9447bdc6a00336b45a4
shigemk2 <i.am.shige@gmail.com>
2016-08-29 00:48:16 +0900

    Add MyActor2

--- src/main/scala/com/example/MyActor2.scala
+++ src/main/scala/com/example/MyActor2.scala
@@ -1,5 +1,24 @@
 package com.example
 
-class MyActor2 {
+import akka.actor.Actor
+import akka.event.Logging
 
+class MyActor2 extends Actor {
+  val log = Logging(context.system, this)
+
+  override def preStart(): Unit = {
+    log.info("preStart")
+  }
+
+  override def postStop(): Unit = {
+    log.info("postStop")
+  }
+
+  def receive = {
+    case s: String => {
+      log.info(s)
+    }
+    case _ => {
+    }
+  }
 }
